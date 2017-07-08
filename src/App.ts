f9fd372b1ba5a783d62f116b4a54dc55ba20c6d6
redaktor <api@redaktorcms.com>
2017-07-08 12:30:28 +0200

    How to set initial state ???
    See the TODO FIXME note and uncomment TODO FIXME - HOW CAN I SET THE
    INITIAL STATE FOR AN ACTIVE PANEL ???

--- src/App.ts
+++ src/App.ts
@@ -1,6 +1,7 @@
+import { mixin } from '@dojo/core/lang';
 import { w, v } from '@dojo/widget-core/d';
 import { DNode, WidgetProperties } from '@dojo/widget-core/interfaces';
-import { WidgetBase } from '@dojo/widget-core/WidgetBase';
+import { WidgetBase, beforeRender } from '@dojo/widget-core/WidgetBase';
 import { theme, ThemeableMixin } from '@dojo/widget-core/mixins/Themeable';
 
 import * as css from './styles/app.m.css';
@@ -8,15 +9,14 @@ import * as css from './styles/app.m.css';
 export interface AccordionTitleProperties { active: boolean; }
 export interface AccordionPanelProperties {
   id: string;
-  exclusive: boolean;
-	onChange: (id: string, e?: any) => void;
   active: boolean;
+  exclusive: boolean;
+	onChange: (evt: any) => void;
 }
 export interface AccordionProperties {
-  exclusive: boolean;
-	onChange: (id: string, e?: any) => void;
-
   panels: any;
+  exclusive: boolean;
+	onChange: (evt: any) => void;
 }
 
 const Base = ThemeableMixin(WidgetBase);
@@ -35,45 +35,67 @@ export class AccordionTitle extends Base<AccordionTitleProperties> {
 }
 
 export class AccordionPanel extends Base<AccordionPanelProperties> {
+    get id() { return this.properties.id }
+
     private _onChange(event: MouseEvent) {
-      this.properties.onChange(this.properties.id, event);
+      this.properties.onChange(mixin({}, event, {widget: this}))
     }
     protected render(): DNode {
         const { id, active, exclusive } = this.properties;
         return v('label', [
           v('input', {
+            checked: active,
             type: !!exclusive ? 'radio' : 'checkbox',
             name: '_g_id',
             onchange: this._onChange
           }),
-          w(AccordionTitle, {active}, [id + ' What is a dog?'])
+          w(AccordionTitle, {active}, [id])
         ]);
     }
 }
 
 export class Accordion extends WidgetBase<AccordionProperties> {
-  private _activeIds: string[] = [];
 
-  private _onAccordionPanelChanged(id: string, e: any): void {
-    console.log('changed', id, e, e.target.checked)
+  static Panel = AccordionPanel;
+  //static Content = AccordionContent;
+  static Title = AccordionTitle;
+  //static Meta = AccordionMeta;
+
+   _activeIds: string[] = [];
+
+  private _onAccordionPanelChanged(e: any): void {
+    console.log('changed', e, e.target.checked, e.widget.id)
     const { exclusive } = this.properties;
     if (exclusive) { this._activeIds = [] }
-    if (e.target.checked) {
-      this._activeIds.push(id)
-    } else if (!exclusive) {
-      const pos = this._activeIds.indexOf(id);
-      pos > -1 && this._activeIds.splice(pos, 1);
+    if (e.target.checked && this._activeIds.indexOf(e.widget.id) < 0) {
+      this._activeIds.push(e.widget.id)
+    } else if (!e.target.checked && !exclusive) {
+      console.log('before remove', this._activeIds)
+      const pos = this._activeIds.indexOf(e.widget.id);
+      if (pos > -1) {
+        this._activeIds.splice(pos, 1);
+      }
     }
     this.invalidate();
+    if (!e.target.checked && !exclusive) {console.log('remove', this._activeIds);}
   }
   private _onAccordionPanelsClosed() {
     this._activeIds = [];
     this.invalidate();
   }
-  protected render(): DNode {
+
+  render(): DNode {
     const { exclusive, panels = [] } = this.properties;
-    const children: DNode[] = panels.map((o: any) => {
+
+    let children: DNode[] = panels.map((o: any) => {
       const id = o.title;
+
+/*  // TODO FIXME - HOW CAN I SET THE INITIAL STATE FOR AN ACTIVE PANEL ???
+      if (o.active && this._activeIds.indexOf(id) < 0) {
+        if (exclusive) { this._activeIds = [] }
+        this._activeIds.push(id);
+      }
+*/
       return w(AccordionPanel, {
         id, exclusive,
         key: id,
@@ -94,18 +116,31 @@ export class Accordion extends WidgetBase<AccordionProperties> {
   }
 }
 
+
 export class App extends WidgetBase<WidgetProperties> {
 
-  protected render(): DNode {
-    return w(Accordion, {
-    	exclusive: false,
-    //				icon: true,
-    //				styled: true,
-    	onChange: (id: string, e: Event) => { console.log(id, e) },
-    	panels: [
-    		{title: 'title', meta: 'meta', content: 'Lorem Ipsum', active: true},
-    		{title: 'title2', meta: 'meta2', content: 'Lorem Ipsum dolor sunt'}
-    	]
-    });
+  protected render(): DNode[] {
+    return [
+      v('span',['Accordion logic for "exclusive: false"']),
+      w(Accordion, {
+      	exclusive: false, /* icon: true, styled: true, etc. */
+      	onChange: (evt: Event) => { console.log(evt) },
+      	panels: [
+      		{title: 'title', meta: 'meta', content: 'Lorem Ipsum', active: true},
+      		{title: 'title2', meta: 'meta2', content: 'Lorem Ipsum dolor sunt'}
+      	]
+      }),
+
+      v('span',['Accordion logic for "exclusive: true" (last radio = close all stub)']),
+      w(Accordion, {
+      	exclusive: true,
+      	onChange: (evt: Event) => { console.log(evt) },
+      	panels: [
+      		{title: 'title', meta: 'meta', content: 'Lorem Ipsum', active: true},
+      		{title: 'title2', meta: 'meta2', content: 'Lorem Ipsum dolor sunt'}
+      	]
+      })
+
+    ];
   }
 }
